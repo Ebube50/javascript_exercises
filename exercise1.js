854d25caabe7e7586c3849c1655a5699ecb68ca3
Ebube Nnaemeka <111168925+Ebube50@users.noreply.github.com>
2026-01-03 18:55:01 +0100

    updated description in exercise1

--- exercise1.js
+++ exercise1.js
@@ -1,16 +1,18 @@
 //this file contains solution to question 1-5
-
+// no 1
 var firstname = 'Ebube';
 var lastname = 'Nnaemeka';
 let age = 27;
 console.log('My name is '+ firstname + ' ' + lastname +' and I am ' + age + ' years old.');
 
+// no 2
 let num1 = 15; num2 = 8;
 console.log('Sum of 15 and 8 is', num1+num2);
 console.log('Difference of 15 and 8 is', num1-num2);
 console.log('Product of 15 and 8 is', num1*num2);
 console.log('Quotient of 15 and 8 is', num1/num2);
 
+// no 3
 var temperature = '22';
 if(temperature < 20) {
     console.log('Its cold');}
@@ -18,9 +20,11 @@ if(temperature < 20) {
 
 }
 
+// no 4
 var price = '50'; var discount = '10';
 console.log('Final price after discount is', price-discount);
 
+// no 5
 let number = 4;
 if(number>0){
     console.log('positive', number);
--- exercise2.js
+++ exercise2.js
@@ -1,2 +1,23 @@
 // this file contains solutions to question 6-10
+//no 6
+var score = '75';
+if(score>=50){
+    console.log('Pass');
+}
+else{
+    console.log('Fail');
+}
 
+//no 7
+var a='12'; var b='15';
+if(a-b>0){
+    console.log('a is greater than b');
+}
+else if(a-b<0){
+    console.log('b is greater than a');
+}
+else{
+    console.log('both are equal')
+}
+
+//no 8
