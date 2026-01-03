060de640443f772cc7f91e1272baa335cd6c93ed
Ebube Nnaemeka <111168925+Ebube50@users.noreply.github.com>
2026-01-03 18:17:13 +0100

    Update exercise1.js

--- exercise1.js
+++ exercise1.js
@@ -1,2 +1,33 @@
 //this file contains solution to question 1-5
 
+var firstname = 'Ebube';
+var lastname = 'Nnaemeka';
+let age = 27;
+console.log('My name is '+ firstname + ' ' + lastname +' and I am ' + age + ' years old.');
+
+let num1 = 15; num2 = 8;
+console.log('Sum of 15 and 8 is', num1+num2);
+console.log('Difference of 15 and 8 is', num1-num2);
+console.log('Product of 15 and 8 is', num1*num2);
+console.log('Quotient of 15 and 8 is', num1/num2);
+
+var temperature = '22';
+if(temperature < 20) {
+    console.log('Its cold');}
+    else{console.log('Its warm');
+
+}
+
+var price = '50'; var discount = '10';
+console.log('Final price after discount is', price-discount);
+
+let number = 4;
+if(number>0){
+    console.log('positive', number);
+}
+else if(number<0){
+console.log('negative', number);
+}
+else{
+    console.log('number is zero');
+}
